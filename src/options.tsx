f5311120708bfff1e9b605380bea135e86076fdd
Puneet Tripathi <puneettripathi13@gmail.com>
2025-01-27 14:34:26 +0530

    Added primitive options and dashboard pages

---
+++ src/options.tsx
@@ -0,0 +1,5 @@
+type Props = {}
+
+export default function options({}: Props) {
+  return <div>options</div>
+}
--- src/popup.tsx
+++ src/popup.tsx
@@ -6,6 +6,11 @@ function IndexPopup() {
   return (
     <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-16 plasmo-w-40 plasmo-bg-red-500">
       {/* <CountButton /> */}
+      <a
+        target="_blank"
+        href="chrome-extension://ldabhphojmofmkcfkhecapkamkgpnmpk/tabs/dashboard.html">
+        Goto Dashboard
+      </a>
     </div>
   )
 }
